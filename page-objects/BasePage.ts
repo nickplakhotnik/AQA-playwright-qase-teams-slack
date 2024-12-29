b6ea779c6cb078618028bebe0e42bd90af27fcee
nickplakhotnik <plakhotnik21@gmail.com>
2024-12-29 19:59:25 +0200

    add base page and constant class

---
+++ page-objects/BasePage.ts
@@ -0,0 +1,33 @@
+import {BrowserContext, Page} from "@playwright/test";
+import playwrightConfig from "../playwright.config";
+
+export class BasePage {
+    readonly page: Page;
+    readonly url: string;
+
+    constructor(page: Page, url: string) {
+        this.page = page;
+        this.url = url;
+    }
+
+    async open() {
+        await this.page.goto(playwrightConfig.use.baseURL.concat(this.url));
+    }
+
+    async waitNetworkKidLoadState() {
+        await this.page.waitForLoadState('networkidle');
+    }
+
+    async waitForDomContentLoad() {
+        await this.page.waitForLoadState('domcontentloaded');
+    }
+
+    async waitForLoadState() {
+        await this.page.waitForLoadState(`load`);
+    }
+
+    async reloadPage() {
+        await this.page.reload();
+        await this.waitForDomContentLoad();
+    }
+}
